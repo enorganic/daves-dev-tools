3608e7f7fd16577f4341ef5117ae68604f372ecb
David Belais <david@belais.me>
2020-12-04 07:43:11 -0800

    *

--- setup.py
+++ setup.py
@@ -35,7 +35,7 @@ def setup(**kwargs) -> None:
 
 setup(
     name="daves-dev-tools",
-    version="0.0.23",
+    version="0.1.0",
     description="Dave's Dev Tools",
     author="David Belais",
     author_email="david@belais.me",
