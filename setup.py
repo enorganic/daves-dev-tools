b701d1d4614504aa9ebd798215e68679d9fbcaac
David Belais <david@belais.me>
2020-12-07 13:07:32 -0800

    *

--- setup.py
+++ setup.py
@@ -1,9 +1,53 @@
+import re
 import sys
-from typing import Set
+from collections import OrderedDict
+from itertools import chain
+from typing import (
+    Dict,
+    Iterable,
+    List,
+    Match,
+    Optional,
+    Pattern,
+    Sequence,
+    Set,
+)
 
 import setuptools
 
 _INSTALL_REQUIRES: str = "install_requires"
+
+
+_extras_pattern: Pattern = re.compile(r"^([^\[]+\[)([^\]]+)(\].*)$")
+
+
+def consolidate_requirement_options(
+    requirements: Iterable[str],
+) -> Iterable[str]:
+    requirement: str
+    templates_options: Dict[str, Set[str]] = OrderedDict()
+    traversed_requirements: Set[str] = set()
+    for requirement in requirements:
+        print(requirement)
+        match: Optional[Match] = _extras_pattern.match(requirement)
+        if match:
+            groups: Sequence[str] = match.groups()
+            no_extras_requirement: str = f"{groups[0][:-1]}{groups[2][1:]}"
+            template: str = f"{groups[0]}{{}}{groups[2]}"
+            if template not in templates_options:
+                templates_options[template] = set()
+            templates_options[template] |= set(groups[1].split(","))
+            if no_extras_requirement in templates_options:
+                del templates_options[no_extras_requirement]
+        elif requirement not in traversed_requirements:
+            templates_options[requirement] = None
+    template: str
+    options: Optional[List[str]]
+    for template, options in templates_options.items():
+        if options:
+            yield template.format(",".join(sorted(options)))
+        else:
+            yield template
 
 
 def setup(**kwargs) -> None:
@@ -20,22 +64,45 @@ def setup(**kwargs) -> None:
             kwargs[_INSTALL_REQUIRES] = []
         kwargs[_INSTALL_REQUIRES].append("dataclasses")
     # Add an "all" extra which includes all extra requirements
-    if "extras_require" in kwargs and "all" not in kwargs["extras_require"]:
-        all_extras_require: Set[str] = set()
-        kwargs["extras_require"]["all"] = []
-        for extra_name, requirements in kwargs["extras_require"].items():
-            if extra_name != "all":
-                for requirement in requirements:
-                    if requirement not in all_extras_require:
-                        all_extras_require.add(requirement)
-                        kwargs["extras_require"]["all"].append(requirement)
+    if "extras_require" in kwargs:
+        if "all" not in kwargs["extras_require"]:
+            kwargs["extras_require"]["all"] = list(
+                consolidate_requirement_options(
+                    chain(*kwargs["extras_require"].values())
+                )
+            )
+        kwargs["extras_require"]["test"] = list(
+            consolidate_requirement_options(
+                chain(
+                    *(
+                        values
+                        for key, values in kwargs["extras_require"].items()
+                        if key not in ("dev", "all")
+                    )
+                )
+            )
+        )
+        print(
+            "extras_require[all]:\n"
+            + "\n".join(
+                f"- {requirement}"
+                for requirement in kwargs["extras_require"]["all"]
+            )
+        )
+        print(
+            "extras_require[test]:\n"
+            + "\n".join(
+                f"- {requirement}"
+                for requirement in kwargs["extras_require"]["test"]
+            )
+        )
     # Pass the modified keyword arguments to `setuptools.setup`
     setuptools.setup(**kwargs)
 
 
 setup(
     name="daves-dev-tools",
-    version="0.2.0",
+    version="0.3.0",
     description="Dave's Dev Tools",
     author="David Belais",
     author_email="david@belais.me",
